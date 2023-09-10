9eecbb8937ab3824f934a18e899342ad4ada376e
josave <josave@odds.team>
2023-09-10 14:20:58 +0700

    Feat class loginPage

---
+++ pom/login.page.ts
@@ -0,0 +1,38 @@
+import { Locator, Page, expect } from "@playwright/test";
+import { validUser } from "../fixtures/User";
+import { app} from '../fixtures/App'
+
+export class loginPage {
+  readonly _page: Page;
+  readonly _usernameInput: Locator;
+  readonly _passwordInput: Locator;
+  readonly _loginButton: Locator;
+  readonly _logoutButton: Locator;
+  readonly _logo: Locator;
+  readonly _logoText: string;
+
+  constructor(page: Page) {
+    this._page = page;
+    this._logoText = "Twittah!";
+    this._usernameInput = page.getByTestId("login-field");
+    this._passwordInput = page.getByTestId("password-field");
+    this._loginButton = page.getByTestId('login-button');
+    this._loginButton = page.getByTestId('menu-signout');
+    this._logo = page.getByTestId('app-name');
+  }
+
+  async getTwittah(){
+    await this._page.goto(app.baseUrl)
+  }
+  async login(){
+    await this._usernameInput.fill(validUser.username);
+    await this._passwordInput.fill(validUser.password);
+    await this._loginButton.click();
+  }
+  async logout(){
+    await this._logoutButton.click();
+  }
+  async shouldBeDisplay(){
+    await expect(this._logo).toHaveText(this._logoText);
+  }
+}
